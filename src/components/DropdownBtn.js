1f225f85a2155f419d2b993dc85c1a4484793380
Derya Antonelli <derya.antonelli@gmail.com>
2021-06-08 11:58:08 +0100

    Rename button component for clarity

---
+++ src/components/DropdownBtn.js
@@ -0,0 +1,12 @@
+import "../Styles/DropdownBtn.scss";
+
+const DropdownBtn = (props) => {
+  return (
+    <button className="dropdown-btn" hidden={props.hidden}>
+      {props.text}
+      <i className="arrow__down"></i>
+    </button>
+  );
+};
+
+export default DropdownBtn;
