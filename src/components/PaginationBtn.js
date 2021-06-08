19380832ca0cddc81385fe1649ac66afa5a70b45
Derya Antonelli <derya.antonelli@gmail.com>
2021-06-08 12:00:30 +0100

    Create pagination button component

---
+++ src/components/PaginationBtn.js
@@ -0,0 +1,15 @@
+import "../Styles/PaginationBtn.scss";
+
+const PaginationBtn = (props) => {
+  return (
+    <button
+      disabled={props.disabled}
+      onClick={props.onClick}
+      className="page__btn"
+    >
+      {props.text}
+    </button>
+  );
+};
+
+export default PaginationBtn;
